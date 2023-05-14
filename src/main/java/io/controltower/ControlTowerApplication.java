591b459e29233b1c94139317eac8e32c440a1124
Leandro <lafs.dev@gmail.com>
2023-05-14 19:42:54 -0300

    Redirect / to /rotas

--- src/main/java/io/controltower/ControlTowerApplication.java
+++ src/main/java/io/controltower/ControlTowerApplication.java
@@ -2,6 +2,10 @@ package io.controltower;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class
@@ -10,6 +14,13 @@ ControlTowerApplication {
     public static void main(String[] args) {
 
         SpringApplication.run(ControlTowerApplication.class, args);
+    }
+
+    @Configuration
+    public static class MvcConfig extends WebMvcConfigurerAdapter {
+        public void addViewControllers(ViewControllerRegistry registry){
+            registry.addRedirectViewController("/", "/rotas");
+        }
     }
 
 }
